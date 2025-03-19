39c20c702b1e78d1aa83c83d98207161a289b13c
gustavorods <gustavo.rlsilva07@gmail.com>
2025-03-19 14:31:56 -0300

    feat: insert

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -15,44 +15,25 @@
     <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact sales</h2>
     <p class="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
   </div>
-  <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+  <form action="{{ url('/cadastrar-produto') }}" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+    @csrf
     <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
       <div>
-        <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">First name</label>
+        <label for="name" class="block text-sm/6 font-semibold text-gray-900">Name</label>
         <div class="mt-2.5">
-          <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+          <input type="text" name="name" id="name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
         </div>
       </div>
       <div>
-        <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">Last name</label>
+        <label for="value" class="block text-sm/6 font-semibold text-gray-900">Value</label>
         <div class="mt-2.5">
-          <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
+          <input type="text" name="value" id="value" autocomplete="family-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
         </div>
       </div>
       <div class="sm:col-span-2">
-        <label for="company" class="block text-sm/6 font-semibold text-gray-900">Address</label>
+        <label for="quantity" class="block text-sm/6 font-semibold text-gray-900">Quantity:</label>
         <div class="mt-2.5">
-          <input type="text" name="Address" id="Address" autocomplete="organization" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
-        </div>
-      </div>
-      <div class="sm:col-span-2">
-        <label for="email" class="block text-sm/6 font-semibold text-gray-900">Email</label>
-        <div class="mt-2.5">
-          <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
-        </div>
-      </div>
-      <div class="sm:col-span-2">
-        <label for="phone-number" class="block text-sm/6 font-semibold text-gray-900">Phone number</label>
-        <div class="mt-2.5">
-          <div class="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-            <input type="text" name="phone-number" id="phone-number" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="123-456-7890">
-          </div>
-        </div>
-      </div>
-      <div class="sm:col-span-2">
-        <label for="message" class="block text-sm/6 font-semibold text-gray-900">Message</label>
-        <div class="mt-2.5">
-          <textarea name="message" id="message" rows="4" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+          <input type="text" name="quantity" id="quantity" autocomplete="organization" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600">
         </div>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Produto; 
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +10,16 @@ Route::get('/', function () {
 
 Route::get('/form', function () {
     return view('form');
+});
+
+Route::post('/cadastrar-produto', function(Request $request) {
+    //dd($request->all());
+
+    Produto::create([
+        'nome' => $request->name,
+        'valor' => $request->value,
+        'estoque' => $request->quantity 
+    ]);
+
+    echo "Produto criado com sucesso!";
 });
